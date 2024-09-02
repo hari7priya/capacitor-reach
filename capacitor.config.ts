73f270f4ffd4177ecae271f9dc8844d422fd81eb
hari7priya <hchinthapally@mednet.ucla.edu>
2024-09-02 03:08:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "org.uclahealth.ctrl.capreach",
+  appName: "capreach",
+  webDir: "build", // This is the directory where the web assets are located
+};
+
+export default config;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+// content updated to include all the js, jsx, ts, tsx files in the src folder
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+};
